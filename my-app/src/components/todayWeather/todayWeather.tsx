f5d8b4e3e7173660127a405dca2a0f0447a83911
hotbreakb <areyhappy12@gmail.com>
2022-02-14 14:29:45 +0900

    Add todayWeather layout

--- my-app/src/components/todayWeather/todayWeather.tsx
+++ my-app/src/components/todayWeather/todayWeather.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styles from "./todayWeather.module.css";
+import svg from "./sunny.svg";
 
-const TodayWeather = () => <h1>TodayWeather</h1>;
+const TodayWeather = () => {
+  return (
+    <section className={styles.container}>
+      <img className={styles.img} src={svg} alt="today icon" />
+      {/* <svg className={styles.img} xmlns="./sunny.svg"></svg> */}
+      <div className={styles.text}>
+        <h3 className={styles.location}>연수구</h3>
+        <h2 className={styles.temperature}>10/-3</h2>
+      </div>
+    </section>
+  );
+};
 
 export default TodayWeather;
