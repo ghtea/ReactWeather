0749885453303aa7d7e000fa8d3c80109e7b1a8e
hotbreakb <areyhappy12@gmail.com>
2022-02-14 13:55:41 +0900

    Connect openWeather in app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./App.module.css";
 import Search from "./components/search/search";
+import TodayWeather from "./components/todayWeather/todayWeather";
+import OpenWeather from "./service/openWeather";
 
-function App() {
+function App({ openWeather }: { openWeather: OpenWeather }) {
+  openWeather.getTodayWeather();
+
   return (
     <div className="App">
       <section className={styles.container}>
         <Search />
+        <TodayWeather />
       </section>
     </div>
   );
--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import OpenWeather from "./service/openWeather";
+
+const openWeather = new OpenWeather();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App openWeather={openWeather} />
   </React.StrictMode>,
   document.getElementById("root")
 );
