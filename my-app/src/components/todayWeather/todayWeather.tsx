70cb4fd9d441a1bdd065e1407cba7f3a7c0acc42
hotbreakb <areyhappy12@gmail.com>
2022-02-14 18:12:17 +0900

    Change openWeather js to ts

--- my-app/src/components/todayWeather/todayWeather.tsx
+++ my-app/src/components/todayWeather/todayWeather.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./todayWeather.module.css";
 import clear from "../../images/clear.svg";
 import OpenWeather from "../../service/openWeather";
 
 const TodayWeather = ({ openWeather }: { openWeather: OpenWeather }) => {
-  const result = openWeather
-    .getTodayWeather() //
-    .then(console.log);
+  const [today, setToday] = useState<Object>(openWeather.getTodayWeather());
+
+  const onClick = () => {
+    openWeather.getTodayWeather().then((result) => setToday(result));
+  };
 
   return (
     <section className={styles.container}>
+      <button onClick={onClick}></button>
       <img className={styles.img} src={clear} alt="today icon" />
       <div className={styles.text}>
-        <h3 className={styles.location}>연수구</h3>
+        {today && <h3 className={styles.location}>{today.name}</h3>}
+        {!today && <h3 className={styles.location}>None</h3>}
         <h2 className={styles.temperature}>10/-3</h2>
       </div>
     </section>
--- my-app/src/service/openWeather.ts
+++ my-app/src/service/openWeather.ts
@@ -1,5 +1,8 @@
 class OpenWeather {
-    constructor(key) {
+    private requestOptions;
+    private loc;
+
+    constructor(private key: string) {
         this.key = key;
         this.requestOptions = {
             method: 'GET',
@@ -8,7 +11,7 @@ class OpenWeather {
         this.loc = "incheon";
     }
 
-    async getTodayWeather() {
+    async getTodayWeather<T>(): Promise<T | null> {
         const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.loc}&appid=${this.key}&units=metric\n`, this.requestOptions)
             .then(response => response.json())
             .catch(error => console.log('error', error));
